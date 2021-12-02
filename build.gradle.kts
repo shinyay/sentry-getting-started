3fd0998f7ae2c288c2cdcbdaf0546138cf384fc8
shinyay <shinya.com@gmail.com>
2021-12-02 22:07:24 +0900

    Add Sentry starter

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,6 +17,7 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("io.sentry:sentry-spring-boot-starter:5.4.3")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
